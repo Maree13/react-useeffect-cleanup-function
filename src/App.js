d94a114162a2e87b3f8ea6d62c2dea18def15efe
Mariana Sarovic <sarovicmariana@gmail.com>
2024-05-26 20:06:22 +0200

    priklad nastaveni casu

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 const App = () => {
   const [currentTime, setCurrentTime] = useState();
 
+  console.log(new Date().toLocaleTimeString());
+
   return (
     <div>
       <h2>Aktuální čas</h2>
@@ -17,3 +19,9 @@ export default App;
 // musime ho jako useState taky naimportovat
 
 // useState a useEffect musime pouzit uvnitr komponenty
+
+// useState umoznuje nastavit promennu, ktera bude mit nejakou hodnotu, a tu hodnotu v te promenne zmenit a tu komponentu znova vypsat.
+
+// To co ulozime do zavorek useState() se nam ulozi do currentTime. setCurrentTime je funkce, kdyz nekam dolu napiseme to setCurrentTime a dame tam obli zavorky a neco posleme dovnitr, tak to co posleme do tech oblich zavorek se nam ulozi do toho currentTime a ta komponenta se znova vypise.
+
+// new Date() ==> Sun May 26 2024 20:02:37 GMT+0200 (Central European Summer Time). My by sme si z toho potrebovali jenom vytahnout ten cas. new Date().toLocaleTimeString(). Je to klasicky js. My ted tohle vemem a napiseme si to do useStatu.
