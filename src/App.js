4806fab352832a6c583ee683103afc19c5c173e7
Mariana Sarovic <sarovicmariana@gmail.com>
2024-05-26 20:10:14 +0200

    currentTime

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { useState, useEffect } from 'react';
 
 const App = () => {
-  const [currentTime, setCurrentTime] = useState();
-
-  console.log(new Date().toLocaleTimeString());
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString);
 
   return (
     <div>
       <h2>Aktuální čas</h2>
+      <p>{currentTime}</p>
     </div>
   );
 };
@@ -24,4 +23,4 @@ export default App;
 
 // To co ulozime do zavorek useState() se nam ulozi do currentTime. setCurrentTime je funkce, kdyz nekam dolu napiseme to setCurrentTime a dame tam obli zavorky a neco posleme dovnitr, tak to co posleme do tech oblich zavorek se nam ulozi do toho currentTime a ta komponenta se znova vypise.
 
-// new Date() ==> Sun May 26 2024 20:02:37 GMT+0200 (Central European Summer Time). My by sme si z toho potrebovali jenom vytahnout ten cas. new Date().toLocaleTimeString(). Je to klasicky js. My ted tohle vemem a napiseme si to do useStatu.
+// new Date() ==> Sun May 26 2024 20:02:37 GMT+0200 (Central European Summer Time). My by sme si z toho potrebovali jenom vytahnout ten cas. new Date().toLocaleTimeString(). Je to klasicky js. My ted tohle vemem a napiseme si to do useStatu. const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString); Pak currentTime vlozim do returnu jako npr. odstavec <p>{currentTime}</p>
