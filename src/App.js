06b2a79f0c70459d46395a4ff2b9678b7ae0b406
Mariana Sarovic <sarovicmariana@gmail.com>
2024-05-28 12:40:21 +0200

    set interval

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { useState, useEffect } from 'react';
 
+setInterval(() => {
+  console.log('test');
+}, 3000);
+
 const App = () => {
   const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString);
+
+  useEffect(() => {});
 
   return (
     <div>
@@ -24,3 +30,7 @@ export default App;
 // To co ulozime do zavorek useState() se nam ulozi do currentTime. setCurrentTime je funkce, kdyz nekam dolu napiseme to setCurrentTime a dame tam obli zavorky a neco posleme dovnitr, tak to co posleme do tech oblich zavorek se nam ulozi do toho currentTime a ta komponenta se znova vypise.
 
 // new Date() ==> Sun May 26 2024 20:02:37 GMT+0200 (Central European Summer Time). My by sme si z toho potrebovali jenom vytahnout ten cas. new Date().toLocaleTimeString(). Je to klasicky js. My ted tohle vemem a napiseme si to do useStatu. const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString); Pak currentTime vlozim do returnu jako npr. odstavec <p>{currentTime}</p>
+
+// aktualni cas vlozim do currentTime a komponenta se musi znova refreshut. Jak zapezpecime, ze se bude komponenta refreshovat kazdou vterinu? aby se cas vypisoval dokola. "Nastartuj mi az na konci to pocitani."- to znamena, ze kazdu vterinu mi ten cas nastav. Na to pouzijem useEffect. useEffect(() => {});
+
+// pouziju setInterval ==> nastavi, jak casto se neco ma opakovat. setInterval(() => {console.log('test');}, 3000); 3000 = opakovani po 3 sec, a do hranatych zavorek napisem co se ma provest. === kazde 3 vteriny se do console vypise 'test'
